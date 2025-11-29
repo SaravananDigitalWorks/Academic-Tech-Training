01682e9327c76809c50146ca565e130b4dab2844
SaravananDigitalWorks <145845495+SaravananDigitalWorks@users.noreply.github.com>
2025-11-29 23:19:00 +0530

    util added

---
+++ django/grocery_store/inventory/utils.py
@@ -0,0 +1,15 @@
+import requests
+
+TELEGRAM_BOT_TOKEN="XXXXXXXXXXXXXXXXXXXXXXX"
+
+TELEGRAM_CHAT_ID="XXXXXXXXXX"
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+    response = requests.post(url, json=payload)
+    return response.json()
